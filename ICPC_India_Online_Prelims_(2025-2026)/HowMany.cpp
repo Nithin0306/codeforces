5d5e175d591f72fc342fcffc3680b00e43416478
Nithin0306 <nithin.p.m2006@gmail.com>
2025-11-30 13:24:13 +0530

    HowMany

---
+++ ICPC_India_Online_Prelims_(2025-2026)/HowMany.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main(){
+    fast_io;
+    int n;
+    cin >> n;
+    cout << (n*3) << "\n";
+    return 0;
+}
