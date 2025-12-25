ac2d576fa8f9bfa3006c7c4f4c96e21ac5b5e483
Nithin0306 <nithin.p.m2006@gmail.com>
2025-12-25 23:57:06 +0530

    1777A

---
+++ 1777A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while (t--){
+        int n;
+        cin >> n;
+
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        
+        int operation_count = 0;
+        for (int i = 0; i < n - 1; i++){
+            if (a[i] % 2 == a[i+1] % 2)
+                operation_count ++;
+        }
+
+        cout << operation_count << endl;
+    }
+    return 0;
+}
