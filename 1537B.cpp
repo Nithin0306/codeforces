79d978b65a083145bba34a97fca7955af2a3edef
Nithin0306 <nithin.p.m2006@gmail.com>
2026-02-02 23:15:57 +0530

    1537B

---
+++ 1537B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+ 
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr); 
+ 
+int main() { 
+    fast_io; 
+ 
+    long long t; 
+    cin >> t; 
+    while (t--) { 
+        // solve here 
+        long long n, m, i ,j ;
+        cin >> n >> m >> i >> j;
+
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    } 
+ 
+    return 0; 
+} 
