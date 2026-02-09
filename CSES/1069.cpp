0597de6a84e1e5f744c620dc80911ce5957e689c
Nithin0306 <nithin.p.m2006@gmail.com>
2026-02-09 19:07:10 +0530

    1069

---
+++ CSES/1069.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    int maxLen = 1;
+    int len = 1;
+
+    for (int i = 0; i < n - 1; i++){
+        if (s[i] == s[i+1]){
+            len++;
+            maxLen = max(maxLen,len);
+        }
+        else
+            len = 1;
+    }
+
+    cout << maxLen << endl;
+
+
+    return 0;
+}
